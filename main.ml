fc5cb664900ff3f972eac8bf83e7c3223548bfc9
David Zmick <dpzmick@gmail.com>
2015-03-06 22:46:13 -0600

    better command line, more flexible for editing

--- main.ml
+++ main.ml
@@ -8,58 +8,99 @@ let list_all_cities g =
 let port_info_for g code =
     let port = Graph.port_for_code g code in
     match port with
-    | None   -> printf "port %s found!\n" code
-    | Some p -> printf "port info for %s\n%s\n" code (Port.string_of_t p)
+    | None   -> printf "port %s not found!\n" code
+    | Some p -> printf "port info for %s\n---\n%s\n" code (Port.string_of_t p)
 
-let print_stats g =
+let port_query g cmd =
+    match cmd with
+    | codes -> List.iter codes ~f:(port_info_for g)
+    | [] -> printf "command error: port codes not specified"
+
+let longest_single_flight_cmd g =
     let longest = Graph.longest_path g in
-    let shortest = Graph.shortest_path g in
-    begin
-        match longest with
-        | None -> printf "There was no longest path"
-        | Some r -> printf "Longest Path: %d\n" (Route.distance r);
+    match longest with
+    | None -> printf "there was no longest flight\n"
+    | Some r -> printf "Longest flight: %s -> %s, distance: %d\n"
+                    (Port.code (Route.from_port r)) (Port.code (Route.to_port r)) (Route.distance r)
 
-        match shortest with
-        | None -> printf "There was no shortest path"
-        | Some r -> printf "Shortest Path: %d\n" (Route.distance r);
+let greatest_query g cmd =
+    match cmd with
+    | "flight"::[] -> longest_single_flight_cmd g
+    | "population"::[] -> printf "Largest Population: %d\n"  (Graph.largest_pop g);
+    | _ -> printf "command error: error to greatest query\n"
 
-        printf "Largest Population: %d\n"  (Graph.largest_pop g);
-        printf "Smallest Population: %d\n" (Graph.smallest_pop g);
-        printf "Average Population: %f\n"  (Graph.average_population g);
-        List.iter (Graph.continents_served g)
-            ~f:(fun (continent, cities) -> begin
-                printf "Cities reachable on continent %s:\n" continent;
-                List.iter cities
-                    ~f:(fun city -> printf "\t%s\n" city)
-            end);
-        match (Graph.hubs g) with
-        | None -> printf "No hubs\n"
-        | Some (hub_connections, hubs) -> begin
-                printf "Hubs with %d connections: " hub_connections;
-                List.iter hubs ~f:(fun hub -> printf "%s, " (Port.name hub));
-                printf "\n";
-        end
+let smallest_single_flight_cmd g =
+    let longest = Graph.shortest_path g in
+    match longest with
+    | None -> printf "there was no shortest flight\n"
+    | Some r -> printf "Shortest flight: %s -> %s, distance: %d\n"
+                    (Port.code (Route.from_port r)) (Port.code (Route.to_port r)) (Route.distance r)
+
+let smallest_query g cmd =
+    match cmd with
+    | "flight"::[] -> smallest_single_flight_cmd g
+    | "population"::[] -> printf "Smallest Population: %d\n"  (Graph.smallest_pop g);
+    | _ -> printf "command error: error to greatest query\n"
+
+let average_query g cmd =
+    match cmd with
+    | "flight"::[] -> printf "not yet implemented\n"
+    | "population"::[] -> printf "Average Population: %f\n"  (Graph.average_population g);
+    | _ -> printf "command error: error to greatest query\n"
+
+let size_query g cmd =
+    match cmd with
+    | "greatest"::remain -> greatest_query g remain
+    | "smallest"::remain -> smallest_query g remain
+    | "average"::remain -> average_query g remain
+    | other::_ -> printf "command error: invalid size query %s\n" other
+    | [] -> printf "command error: no size subquery given\n"
+
+let print_continents g =
+    List.iter (Graph.continents_served g)
+        ~f:(fun (continent, cities) -> begin
+            printf "Cities reachable on continent %s:\n" continent;
+            List.iter cities
+                ~f:(fun city -> printf "\t%s\n" city)
+    end)
+
+let print_hubs g =
+    match (Graph.hubs g) with
+    | None -> printf "No hubs\n"
+    | Some (hub_connections, hubs) -> begin
+        printf "Hubs with %d connections: " hub_connections;
+        List.iter hubs ~f:(fun hub -> printf "%s, " (Port.name hub));
+        printf "\n";
     end
 
-let command g =
-    Command.basic
-        ~summary: "Query the flight database"
-        ~readme:(fun () -> "halp")
-        Command.Spec.(
-            empty
-            +> flag "-L" no_arg ~doc: " list all the cities CSAir flies to"
-            +> flag "-P" (optional string) ~doc:"code get information about a specific port (given by port code)"
-            +> flag "-S" no_arg ~doc:" get statistical information about the CSAir network"
-            +> flag "-M" no_arg ~doc:" open great circle mapper with this map"
-        )
-        (fun list_cities port_info stats gcm () -> begin
-            if list_cities                then (list_all_cities g);
-            if (Option.is_some port_info) then (port_info_for g (Option.value_exn port_info));
-            if stats                      then (print_stats g);
-            if gcm                        then printf "return: %d\n"
-                                                (Sys.command (sprintf "open %s" (Gcm_data.string_of_t (Gcm_data.from g))));
-        end)
+let list_query g cmd =
+    match cmd with
+    | "hubs"::[] -> print_hubs g
+    | "continents"::[] -> print_continents g
+    | _ -> printf "command error: list query error\n"
+
+let info_query g cmd =
+    match cmd with
+    | "size"::remain -> size_query g remain
+    | "list"::remain -> list_query g remain
+    | other::_ -> printf "command error: invalid info query %s\n" other
+    | [] -> printf "command error: no info subquery given\n"
+
+let query_cmd g cmd =
+    match cmd with
+    | "ports"::remain -> port_query g remain
+    | "info"::remain -> info_query g remain
+    | other::_ -> printf "command error: query %s is not a valid query\n" other
+    | [] -> printf "command error: no query given\n"
+
+let edit_cmd g cmd = printf "edit cmd\n"
+
+let command g cmd =
+    match cmd with
+    | _::"query"::remain -> query_cmd g remain
+    | _::"edit"::remain  -> edit_cmd  g remain
+    | _ -> printf "error: command not recognized\n"
 
 let () =
     let g = Graph.t_of_dataset (Map_data_j.dataset_of_string (In_channel.read_all "map_data.json")) in
-    Command.run (command g)
+    command g (Array.to_list Sys.argv)
